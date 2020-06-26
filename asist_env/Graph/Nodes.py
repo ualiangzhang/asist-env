33177ad89a0dfcd32a97df910b83093560925962
wangyz1999 <yunzhewa@usc.edu>
2020-06-26 13:53:41 -0700

    add connections

--- asist_env/Graph/Nodes.py
+++ asist_env/Graph/Nodes.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 from enum import Enum
 
 
@@ -13,7 +12,11 @@ class VictimType(Enum):
     Safe = 2
     Dead = 3
 
-class Node(ABC):
+class Node:
+    """
+        Super class Node in Graph, three subtypes of Node
+        PortalNode, VictimNode, and RoomNode
+    """
     def __init__(self, id, name):
         self.id = id
         self.name = name
@@ -26,11 +29,11 @@ class Node(ABC):
 class PortalNode(Node):
     """
         portal Node id has format PID-RID (Portal ID - Room ID)
-        for example,
+        Portal also comes in pairs, linked_portal_id is the portal it is connecting
     """
     def __init__(self, id, name, linked_portal_id, location):
         assert linked_portal_id is None or isinstance(linked_portal_id, str)
-        super(Node, self).__init__(id, name)
+        super().__init__(id, name)
         self.type = NodeType.Portal
         self.linked_portal_id = linked_portal_id
         self.loc = location
@@ -41,7 +44,7 @@ class PortalNode(Node):
 class VictimNode(Node):
     def __init__(self, id, name, victim_type, location):
         assert isinstance(victim_type, VictimType)
-        super(Node, self).__init__(id, name)
+        super().__init__(id, name)
         self.type = NodeType.Victim
         self.victim_type = victim_type
         self.loc = location
@@ -49,7 +52,13 @@ class VictimNode(Node):
 
 class RoomNode(Node):
     def __init__(self, id, name, location):
-        super(Node, self).__init__(id, name)
+        super().__init__(id, name)
         self.type = NodeType.Room
         self.loc = location
+        self.victim_list = []
+
+    def add_victim(self, victim_id):
+        assert isinstance(victim_id, str) or isinstance(victim_id, list) and \
+               all(isinstance(v, str) for v in victim_id)
+        self.victim_list += victim_id
 
--- asist_env/Graph/__init__.py
+++ asist_env/Graph/__init__.py
@@ -1,6 +1,7 @@
 import networkx
 from .Nodes import *
 from collections.abc import Iterable
+from math import sqrt
 
 
 class Graph(networkx.Graph):
@@ -35,6 +36,7 @@ class Graph(networkx.Graph):
         :param id: the victim id, if id not give, the method will auto generate one
         :param name: the name of the Victim such as Jason. (Default None)
         :param victim_type: Must be one of [Yellow, Green, Dead, Safe]
+        :param location: location of the victim, tuple of x,z coordinate
         :return: the victim node constructed
         """
         assert id is None or isinstance(id, str)
@@ -72,46 +74,31 @@ class Graph(networkx.Graph):
 
         :param id: the portal id, if id not give, the method will auto generate one
         :param name: name of the portal, if any
-        :param is_pair: if it is a pair of portals. The start is not pair
         :param connected_room: the room that the portal is connected to
+        :param location: location of the portal, tuple of x,z coordinate
         :return: the created portal node
         """
         assert id is None or isinstance(id, str)
         assert name is None or isinstance(name, str)
-        assert isinstance(is_pair, bool)
         assert location is None or isinstance(location, tuple) and \
                len(location) == 2 and all(isinstance(l, float) for l in location)
-        assert not is_pair and isinstance(connected_room_ids, str) or is_pair and \
-               isinstance(connected_room_ids, tuple) and all(isinstance(r, str) for r in connected_room_ids)
+        assert isinstance(connected_room_ids, tuple) and all(isinstance(r, str) for r in connected_room_ids)
 
         node_id = id if id is not None else "P" + str(len(self.portal_list))
 
-        # The case when this is a pair of portals, connecting two rooms
-        if is_pair:
-            node_id_1 = node_id + "-" + connected_room_ids[0]
-            node_id_2 = node_id + "-" + connected_room_ids[1]
+        node_id_1 = node_id + "-" + connected_room_ids[0]
+        node_id_2 = node_id + "-" + connected_room_ids[1]
 
-            node_1 = PortalNode(node_id_1, name, node_id_2, location)
-            node_2 = PortalNode(node_id_2, name, node_id_1, location)
+        node_1 = PortalNode(node_id_1, name, node_id_2, location)
+        node_2 = PortalNode(node_id_2, name, node_id_1, location)
 
-            self.portal_list.append((node_1, node_2))
-            self.nodes_list.append(node_1)
-            self.nodes_list.append(node_2)
-            self.id2node[node_id_1] = node_1
-            self.id2node[node_id_2] = node_2
+        self.portal_list.append((node_1, node_2))
+        self.nodes_list.append(node_1)
+        self.nodes_list.append(node_2)
+        self.id2node[node_id_1] = node_1
+        self.id2node[node_id_2] = node_2
 
-            return node_1, node_2
-
-        # The case when it is a single portal, which is usually the start node
-        else:
-            node_id += "-" + connected_room_ids
-            node = PortalNode(node_id, name, None)
-
-            self.portal_list.append(tuple(node))
-            self.nodes_list.append(node)
-            self.id2node[node_id] = node
-
-            return node
+        return node_1, node_2
 
 
     def add_room(self, id=None, name=None, location=None):
@@ -119,6 +106,7 @@ class Graph(networkx.Graph):
 
         :param id: the room id, if id not give, the method will auto generate one
         :param name: name of the room, if any
+        :param location: location of the center of the room, tuple of x,z coordinate
         :return: the created room node
         """
         assert id is None or isinstance(id, str)
@@ -127,7 +115,7 @@ class Graph(networkx.Graph):
                len(location) == 2 and all(isinstance(l, float) for l in location)
 
         node_id = id if id is not None else "R" + str(len(self.room_list))
-        node = RoomNode(node_id, name)
+        node = RoomNode(node_id, name, location)
 
         self.room_list.append(node)
         self.nodes_list.append(node)
@@ -135,13 +123,60 @@ class Graph(networkx.Graph):
 
         return node
 
+    def connect_portal_to_rooms(self, portal_tuple):
+        """ Connect the portal to the two rooms it is adjacent to
+        :param portal_tuple: the two portals indicate two sides of the door
+        """
+        assert isinstance(portal_tuple, tuple) and len(portal_tuple) == 2 and \
+               all(isinstance(p, PortalNode) for p in portal_tuple)
+
+        portal_1, portal_2 = portal_tuple
+
+        # connecting portal with the two adjacent rooms
+        room_1 = self.id2node[portal_1.get_connected_room_id()]
+        room_2 = self.id2node[portal_2.get_connected_room_id()]
+        self.add_edge(portal_1, room_1, weight=self.euclidean_distances(room_1.loc, portal_1.loc))
+        self.add_edge(portal_2, room_2, weight=self.euclidean_distances(room_2.loc, portal_2.loc))
+
+        # connecting portal with all the victims in side the adjacent room
+        for v_id in room_1.victim_list:
+            victim = self.id2node[v_id]
+            self.add_edge(room_1, victim, weight=self.euclidean_distances(room_1.loc, victim.loc))
+
+        for v_id in room_2.victim_list:
+            victim = self.id2node[v_id]
+            self.add_edge(room_2, victim, weight=self.euclidean_distances(room_2.loc, victim.loc))
+
 
     def link_victims_in_room(self, room, list_of_victim_id):
+        """ Make a fully connected sub-graph of room nodes and victims node inside that room
+
+        :param room: the room Node
+        :param list_of_victim_id: the list of victim ids inside the room
+        :return: the room node
+        """
         assert isinstance(room, RoomNode)
         assert isinstance(list_of_victim_id, list) and all(isinstance(v, str) for v in list_of_victim_id)
 
+        room.add_victim(list_of_victim_id)
+        for v_id in list_of_victim_id:
+            victim = self.id2node[v_id]
+            self.add_edge(room, victim, weight=self.euclidean_distances(room.loc, victim.loc))
 
+        for i in range(len(list_of_victim_id)):
+            for j in range(i+1, len(list_of_victim_id)):
+                victim_1 = self.id2node[list_of_victim_id[i]]
+                victim_2 = self.id2node[list_of_victim_id[j]]
+                self.add_edge(victim_1, victim_2, weight=self.euclidean_distances(victim_1.loc, victim_2.loc))
+
+        return room
 
 
     def get_neighbor(self):
         pass
+
+    @staticmethod
+    def euclidean_distances(pos1, pos2):
+        assert isinstance(pos1, tuple) and isinstance(pos2, tuple)
+        return max(1, int(sqrt((pos2[0] - pos1[0]) ** 2 + (pos2[1] - pos1[1]) ** 2)))
+
